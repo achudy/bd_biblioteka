fb5b497ed0378d73d978a5e84c4307188a33d861
guv4 <chudy.arkadiusz@gmail.com>
2020-12-26 19:17:08 +0100

    all raw methods, without working category filtering

--- README.md
+++ README.md
@@ -3,22 +3,216 @@
 ## GET
 
 #### GET / 
+Gets you a classic youtube video.
+
 #### GET /books 
-#### GET /books/filter?title=cip 
-#### GET /availability?title=Wielka%20Księga%20Cipek&author=Dan%20Hojer,%20Gunilla%20Kvarnstrom 
-#### GET /availability?title=Wielka%20Księga%20Cipek&author=Stanisław%20Lem 
-#### GET /availability?title=Solaris&author=Stanisław%20Lem 
-#### GET /availability?title=Wielka%20Księga%20Cipek&author=Dan%20Hojer,%20Gunilla%20Kvarnstrom 
-#### GET /availability?title=Podróznik%20WC%20&author=Wojciech%20Cejrowski 
-#### GET /availability?title=Podróznik%20WC&author=Wojciech%20Cejrowski 
-#### GET /books 
-#### GET /availability?title=Coś%20się%20kończy,%20coś%20się%20zaczyna&author=Andrzej%20Sapkowski 
+Gets you all the books.
+
+#### GET /books/filter
+Parameters:
+* id (int)
+* title (str)
+* author (str)
+
+Example usage:
+* /books/filter?title=ci&author=pa
+
+Gets you only the books with "cip" in the title and "pa" in author's name.
+
+#### GET /availability
+Parameters:
+* title (str)
+* author (str)
+
+Example usage:
+* /availability?title=Solaris&author=Stanisław%20Lem
+
+Gets you the availability of Stanisław Lem's "Solaris"
+
 #### GET /borrowed 
-#### GET /borrowed/user?login=achudy 
-#### GET /borrowed/id?id=1 
+Gets you all the borrowed books.
+
+#### GET /borrowed/user
+Parameters:
+* login (str)
+
+Example usage:
+* /borrowed/user?login=achudy 
+
+Gets you all books borrowed by the user with login "achudy"
+
+#### GET /borrowed/id
+Parameters:
+* id (int)
+
+Example usage:
+* /borrowed/id?id=1 
+
+Gets you the borrowed book with id=1
+
 #### GET /categories 
-#### GET /categories/book?title=Coś%20się%20kończy,%20coś%20się%20zaczyna&author=Andrzej%20Sapkowski 
+Gets you all the categories of books.
+
+#### GET /categories/book
+Parameters:
+* title (str)
+* author (str)
+
+Example usage:
+* /categories/book?title=Coś%20się%20kończy,%20coś%20się%20zaczyna&author=Andrzej%20Sapkowski
+
+Gets you the category of Andrzej Sapkowski's "C.s.k.c.s.z."
+
 #### GET /branches 
+Gets you all the library branches.
+
+#### GET /branches/id
+Parameters:
+* id (int)
+
+Example usage:
+* /branches/id?id=1 
+
+Gets you only the branch with id=1.
+
 #### GET /users 
-#### GET /branches/id?id=1 
-#### GET /users/user?login=achudy 
+Gets you all the users.
+
+#### GET /users/user
+Parameters:
+* login (str)
+
+Example usage:
+* /users/user?login=achudy 
+
+Gets you only the user with login "achudy"
+
+
+## POST
+
+#### POST /books
+Form data: 
+* title (str)
+* author (str)
+* for_adults (int)
+* library_branch (int)
+* category_names (str) 
+
+Posts a book.
+
+#### POST /borrowed
+Form data: 
+* user_id (int)
+* book_instance_id (int)
+* start_time (date e.g. 2021-06-09)
+* end_time (date)
+
+Posts a borrowed record.
+
+#### POST /categories
+Form data: 
+* category_name (str)
+
+Posts a category.
+
+#### POST /branches
+Form data: 
+* address (str)
+* library_branch_name (str)
+
+Posts a library branch.
+
+#### POST /users
+Form data: 
+* name (str)
+* surname (str)
+* login (str)
+* password (str)
+* birth_date (date)
+* user_type (str)
+
+Posts a user.
+
+## PUT
+
+#### PUT /books
+Form data: 
+* id (int)
+* author (str)
+* for_adults (str)
+* title (int)
+
+Updates a book.
+
+#### PUT /borrowed
+Form data: 
+* id (int)
+* user_id (int)
+* book_instance_id (int)
+* start_time (date e.g. 2021-06-09)
+* end_time (date)
+
+Updates a borrowed record.
+
+#### PUT /categories
+Form data: 
+* id (int)
+* category_name (str)
+
+Updates a category.
+
+#### PUT /branches
+Form data: 
+* id (int)
+* address (str)
+* library_branch_name (str)
+
+Updates a library branch.
+
+#### PUT /users
+Form data: 
+* id (int)
+* name (str)
+* surname (str)
+* login (str)
+* password (str)
+* birth_date (date)
+* user_type (str)
+
+Updates a user.
+
+## DELETE
+
+#### DELETE /books
+Parameters:
+* id (int)
+
+Example usage:
+* /books?id=1
+
+Deletes a book.
+
+#### DELETE /borrowed
+Parameters:
+* id (int)
+
+Deletes a borrowed record.
+
+#### DELETE /categories
+Parameters:
+* id (int)
+
+Deletes a category.
+
+#### DELETE /branches
+Parameters:
+* id (int)
+
+Deletes a library branch.
+
+#### DELETE /users
+Parameters:
+* id (int)
+
+Deletes a user.
+
--- __init__.py
+++ __init__.py
@@ -11,25 +11,48 @@ def query_to_dict(ret):
 
 def create_app():
     app = Flask(__name__)
-    # TODO if running this on your own - change the line below accordingly
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://testowy:password@127.0.0.1/books'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://g15:{pwd}@127.0.0.1/g15'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://g15:{password}@127.0.0.1/g15'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.config['PROPAGATE_EXCEPTIONS'] = True
 
     db = SQLAlchemy(app)
 
-    @app.route('/')
+    @app.route('/', methods=['GET'])
     def this_is_library():
         return 'https://www.youtube.com/watch?v=9Wq54dK0aBs&ab_channel=KING5'
 
-    @app.route('/books')
-    def get_books():
-        result = query_to_dict(db.session.execute('select * from books;'))
-        return result
+    @app.route('/books', methods=['GET', 'POST', 'PUT', 'DELETE'])
+    def books():
+        if request.method == 'GET':
+            result = query_to_dict(db.session.execute('select * from books;'))
+            return result
+        if request.method == 'POST':
+            title = request.form.get('title', None)
+            author = request.form.get('author', None)
+            for_adults = request.form.get('for_adults', None)
+            library_branch = request.form.get('library_branch', None)
+            category_names = request.form.get('category_names', None)
+            db.session.execute(
+                f"call add_new_book('{title}', '{author}', {for_adults}, {library_branch}, '{category_names}');")
+            db.session.commit()
+            return {"status": "OK"}
+        if request.method == 'PUT':
+            id = request.form.get('id', None)
+            author = request.form.get('author', None)
+            for_adults = request.form.get('for_adults', None)
+            title = request.form.get('title', None)
+            db.session.execute(
+                f"update books set author='{author}', for_adults={for_adults}, title='{title}' where id={id};")
+            db.session.commit()
+            return {"status": "OK"}
+        if request.method == 'DELETE':
+            id = request.args.get('id', None)
+            db.session.execute(f"delete from books where id={id};")
+            db.session.commit()
+            return {"status": "OK"}
 
-    @app.route('/books/filter')
-    def get_book_id():
+    @app.route('/books/filter', methods=['GET'])
+    def book_id():
         id = request.args.get('id', None)
         title = request.args.get('title', None)
         category = request.args.get('category', None)
@@ -37,89 +60,184 @@ def create_app():
 
         query = 'select * from books where '
         and_specifier = False
-
         if id is not None:
             query += f'id={id} '
             and_specifier = True
-
         if title is not None:
             if and_specifier:
                 query += 'and '
             filtered = "'%%" + title + "%%'"
             query += f'title like {filtered} '
             and_specifier = True
-
         if author is not None:
             if and_specifier:
                 query += 'and '
             filtered = "'%%" + author + "%%'"
             query += f'author like {filtered} '
             and_specifier = True
-
         if category is not None:
             if and_specifier:
                 query += 'and '
             query += f'fk_category_id = {category} '
-            and_specifier = True
-
+            # and_specifier = True
         query += ';'
 
         result = query_to_dict(db.session.execute(query))
         return result
 
-    @app.route('/availability')
+    @app.route('/availability', methods=['GET'])
     def get_availability():
         title = request.args.get('title', None)
         author = request.args.get('author', None)
         result = query_to_dict(db.session.execute(f"CALL check_availability('{title}', '{author}');"))
         return result
 
-    @app.route('/borrowed')
-    def get_borrowed():
-        result = query_to_dict(db.session.execute('select * from borrowed;'))
-        return result
+    @app.route('/borrowed', methods=['GET', 'POST', 'PUT', 'DELETE'])
+    def borrowed():
+        if request.method == 'GET':
+            result = query_to_dict(db.session.execute('select * from borrowed;'))
+            return result
+        if request.method == 'POST':
+            user_id = request.form.get('user_id', None)
+            book_instance_id = request.form.get('book_instance_id', None)
+            start_time = request.form.get('start_time', None)
+            end_time = request.form.get('end_time', None)
+            db.session.execute(
+                f"insert into borrowed (user_id, book_instance_id, start_time, end_time) " +
+                f"values ({user_id}, {book_instance_id}, '{start_time}', '{end_time}');")
+            db.session.commit()
+            return {"status": "OK"}
+        if request.method == 'PUT':
+            id = request.form.get('id', None)
+            user_id = request.form.get('user_id', None)
+            book_instance_id = request.form.get('book_instance_id', None)
+            start_time = request.form.get('start_time', None)
+            end_time = request.form.get('end_time', None)
+            db.session.execute(
+                f"update borrowed set user_id={user_id}, book_instance_id={book_instance_id}, " +
+                f"start_time='{start_time}', end_time='{end_time}' where id={id};")
+            db.session.commit()
+            return {"status": "OK"}
+        if request.method == 'DELETE':
+            id = request.args.get('id', None)
+            db.session.execute(f"delete from borrowed where id={id};")
+            db.session.commit()
+            return {"status": "OK"}
 
-    @app.route('/borrowed/user')
+    @app.route('/borrowed/user', methods=['GET'])
     def get_borrowed_user():
         login = request.args.get('login', None)
         result = query_to_dict(db.session.execute(f"CALL get_users_books('{login}');"))
         return result
 
-    @app.route('/borrowed/id')
+    @app.route('/borrowed/id', methods=['GET'])
     def get_borrowed_id():
         id = request.args.get('id', None)
         result = query_to_dict(db.session.execute(f"select * from borrowed where id={id};"))
         return result
 
-    @app.route('/categories')
-    def get_categories():
-        result = query_to_dict(db.session.execute('select * from categories;'))
-        return result
+    @app.route('/categories', methods=['GET', 'POST', 'PUT', 'DELETE'])
+    def categories():
+        if request.method == 'GET':
+            result = query_to_dict(db.session.execute('select * from categories;'))
+            return result
+        if request.method == 'POST':
+            category_name = request.form.get('category_name', None)
+            db.session.execute(
+                f"insert into categories (category_name) values ('{category_name}');")
+            db.session.commit()
+            return {"status": "OK"}
+        if request.method == 'PUT':
+            id = request.form.get('id', None)
+            category_name = request.form.get('category_name', None)
+            db.session.execute(
+                f"update categories set category_name='{category_name}' where id={id};")
+            db.session.commit()
+            return {"status": "OK"}
+        if request.method == 'DELETE':
+            id = request.args.get('id', None)
+            db.session.execute(f"delete from categories where id={id};")
+            db.session.commit()
+            return {"status": "OK"}
 
-    @app.route('/categories/book')
+    @app.route('/categories/book', methods=['GET'])
     def get_categories_book():
         title = request.args.get('title', None)
         author = request.args.get('author', None)
         result = query_to_dict(db.session.execute(f"CALL get_category_of_book('{title}', '{author}');"))
         return result
 
-    @app.route('/branches')
-    def get_branches():
-        result = query_to_dict(db.session.execute('select * from library_branches;'))
-        return result
+    @app.route('/branches', methods=['GET', 'POST', 'PUT', 'DELETE'])
+    def branches():
+        if request.method == 'GET':
+            result = query_to_dict(db.session.execute('select * from library_branches;'))
+            return result
+        if request.method == 'POST':
+            address = request.form.get('address', None)
+            library_branch_name = request.form.get('library_branch_name', None)
+            db.session.execute(
+                f"insert into library_branches (address,library_branch_name) " +
+                f"values ('{address}', '{library_branch_name}');")
+            db.session.commit()
+            return {"status": "OK"}
+        if request.method == 'PUT':
+            id = request.form.get('id', None)
+            address = request.form.get('address', None)
+            library_branch_name = request.form.get('library_branch_name', None)
+            db.session.execute(
+                f"update library_branches set address='{address}', library_branch_name='{library_branch_name}'" +
+                f" where id={id};")
+            db.session.commit()
+            return {"status": "OK"}
+        if request.method == 'DELETE':
+            id = request.args.get('id', None)
+            db.session.execute(f"delete from library_branches where id={id};")
+            db.session.commit()
+            return {"status": "OK"}
 
-    @app.route('/branches/id')
+    @app.route('/branches/id', methods=['GET'])
     def get_branches_id():
         id = request.args.get('id', None)
         result = query_to_dict(db.session.execute(f"select * from library_branches where id={id};"))
         return result
 
-    @app.route('/users')
+    @app.route('/users', methods=['GET', 'POST', 'PUT', 'DELETE'])
     def get_users():
-        result = query_to_dict(db.session.execute('select * from users;'))
-        return result
+        if request.method == 'GET':
+            result = query_to_dict(db.session.execute('select * from users;'))
+            return result
+        if request.method == 'POST':
+            name = request.form.get('name', None)
+            surname = request.form.get('surname', None)
+            login = request.form.get('login', None)
+            password = request.form.get('password', None)
+            birth_date = request.form.get('birth_date', None)
+            user_type = request.form.get('user_type', None)
+            db.session.execute(
+                f"insert into users (name,surname,login,password,birth_date,user_type) " +
+                f"values ('{name}', '{surname}', '{login}', '{password}', '{birth_date}', '{user_type}');")
+            db.session.commit()
+            return {"status": "OK"}
+        if request.method == 'PUT':
+            id = request.form.get('id', None)
+            name = request.form.get('name', None)
+            surname = request.form.get('surname', None)
+            login = request.form.get('login', None)
+            password = request.form.get('password', None)
+            birth_date = request.form.get('birth_date', None)
+            user_type = request.form.get('user_type', None)
+            db.session.execute(
+                f"update users set name='{name}', surname='{surname}', login='{login}', " +
+                f"password='{password}', birth_date='{birth_date}', user_type='{user_type}' where id={id};")
+            db.session.commit()
+            return {"status": "OK"}
+        if request.method == 'DELETE':
+            id = request.args.get('id', None)
+            db.session.execute(f"delete from users where id={id};")
+            db.session.commit()
+            return {"status": "OK"}
 
-    @app.route('/users/user')
+    @app.route('/users/user', methods=['GET'])
     def get_users_user():
         login = request.args.get('login', None)
         result = query_to_dict(db.session.execute(f"select * from users where login='{login}';"))
